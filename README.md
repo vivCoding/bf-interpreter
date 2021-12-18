a82520fc128cce69e0c4a3e8720d6f12d1170fd9
vuser <vivCoding@gmail.com>
2021-12-17 19:23:07 -0500

    basically works

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# bf-interpreter
-brainfuck interpreter
+# Brainfuck interpreter
+Takes a file and runs [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) code. Interpreter written in C.
+
+### Usage
+Compile using `gcc` or any C compiler. Then run with file path as parameter
+```
+gcc intrepreter.c -o bf
+./bf <path_to_file>
+```
+
+### Example
+
+```
+./bf hello_world.bf
+```
---
+++ intrepreter.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+
+#define DATA_SIZE 30000
+char* arr[DATA_SIZE];
+int ptr = 0;
+FILE* file;
+
+void skip_to_close() {
+    char c = fgetc(file);
+    while (c != EOF) {
+        if (c == '[') skip_to_close();
+        else if (c == ']') return;
+        c = fgetc(file);
+    }
+}
+
+void execute_bf() {
+    char c = fgetc(file);
+    while (c != EOF) {
+        switch (c) {
+            case '>': ptr++; break;
+            case '<': ptr--; break;
+            case '+': arr[ptr]++; break;
+            case '-': arr[ptr]--; break;
+            case '.': putchar((int) arr[ptr]); break;
+            case ',': arr[ptr] = (char) getchar(); break;
+            case '[':
+                int starting = ftell(file);
+                if (arr[ptr] == 0) {
+                    skip_to_close();
+                }
+                while (arr[ptr] != 0) {
+                    fseek(file, starting, SEEK_SET);
+                    execute_bf();
+                }
+                break;
+            case ']': return;
+        }
+        c = fgetc(file);
+    }
+}
+
+int main (int argc, char* argv[]) {
+    if (argc == 1) {
+        printf("Error: No file specified!\n");
+        return 1;
+    }
+    file = fopen(argv[1], "r");
+    if (!file) {
+        printf("Error: Cannot open file. Make sure it exists!");
+        return 1;
+    }
+
+    execute_bf();
+    return 0;
+}
